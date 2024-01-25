17eb9911626c0b3051b417294ef0101695b68a36
David Montesdeoca <backpackerhh@gmail.com>
2024-01-25 19:18:01 +0100

    Allow to have basic Express server running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi!");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response): void => {
+  res.send("Hi there!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
