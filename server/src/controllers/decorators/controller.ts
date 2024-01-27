37425585a25bc449e78433a9bf88875bbb8f4d8e
David Montesdeoca <backpackerhh@gmail.com>
2024-01-27 12:50:05 +0100

    Allow access to correct path for login
    This is a temporary solution, as the get route should not be hardcoded.

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -9,7 +9,11 @@ export function controller(routePrefix: string) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata("path", target.prototype, key);
 
-      if (path) {
+      if (!path) return;
+
+      if (routePrefix === "/") {
+        router.get(path, routeHandler);
+      } else {
         router.get(`${routePrefix}${path}`, routeHandler);
       }
     }
