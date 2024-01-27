c121059baa9c88e61df216cda3100562cc719772
David Montesdeoca <backpackerhh@gmail.com>
2024-01-27 16:23:05 +0100

    Refactor code to define route decorators

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,7 +1,16 @@
 import "reflect-metadata";
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata("path", path, target, key);
+function defineRouteDecorator(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", method, target, key);
+    };
   };
 }
+
+export const get = defineRouteDecorator("get");
+export const post = defineRouteDecorator("post");
+export const put = defineRouteDecorator("put");
+export const destroy = defineRouteDecorator("delete");
+export const patch = defineRouteDecorator("patch");
